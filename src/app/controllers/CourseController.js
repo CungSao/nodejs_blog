26b8713500f917763e9c0b879e3874da3b9e28c3
CungSao <2022bevy@gmail.com>
2024-02-07 17:06:34 +0700

    Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -8,6 +8,24 @@ class CourseController {
       .then((course) => res.render('courses/show', { course: mongooseToObject(course) }))
       .catch(next);
   }
+
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render('courses/create');
+  }
+
+  // [POST] /courses/store
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(formData);
+    course
+      .save()
+      .then(() => res.redirect('/'))
+      .catch((error) => {});
+
+    res.send('Course created');
+  }
 }
 
 module.exports = new CourseController();
