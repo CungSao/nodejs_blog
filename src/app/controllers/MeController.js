be44c2232e38b9674ae9d87e2f4c2fe849b57b91
CungSao <2022bevy@gmail.com>
2024-02-07 22:03:41 +0700

    soft delete - chưa xog

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,10 +3,21 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class MeController {
   // [GET] /me/store/courses
-  storeCourses(req, res, next) {
-    Course.find({})
+  storedCourses(req, res, next) {
+    Course.find({ })
       .then((courses) =>
         res.render('me/stored-courses', {
+          courses: multipleMongooseToObject(courses),
+        }),
+      )
+      .catch(next);
+  }
+
+  // [GET] /me/trash/courses
+  trashCourses(req, res, next) {
+    Course.findDeleted({ })
+      .then((courses) =>
+        res.render('me/trash-courses', {
           courses: multipleMongooseToObject(courses),
         }),
       )
